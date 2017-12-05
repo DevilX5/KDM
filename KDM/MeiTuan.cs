99dcd4eb8b82c6e6c3ffb1bff0ffaf96b99575d0
DevilX <devilx5@qq.com>
2017-12-05 19:25:29 +0800

    添加项目文件。

---
+++ KDM/MeiTuan.cs
@@ -0,0 +1,40 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace KDM
+{
+    public class FormParam
+    {
+        public int areaId { get; set; }
+        public int cateId { get; set; }
+        public string deal_attr_23 { get; set; }
+        public string deal_attr_24 { get; set; }
+        public string deal_attr_25 { get; set; }
+        public int limit { get; set; }
+        public int lineId { get; set; }
+        public int offset { get; set; }
+        public string poi_attr_20033 { get; set; }
+        public string poi_attr_20043 { get; set; }
+        public string sort { get; set; }
+        public int stationId { get; set; }
+    }
+    public class MeiTuan
+    {
+        public MtLst GetDatas()
+        {
+            var payload = new FormParam() { areaId = 0, cateId = 1, deal_attr_23 = "", deal_attr_24 = "", deal_attr_25 = "", limit = 15, lineId = 0, offset = 0, poi_attr_20033 = "", poi_attr_20043 = "", sort = "default", stationId = 0 };
+            var client = new RestClient();
+            client.BaseUrl = new Uri("http://meishi.meituan.com");
+            client.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36";
+            var request = new RestRequest("/i/api/channel/deal/list",Method.POST);
+            request.AddHeader("Referer", "http://meishi.meituan.com/i/?ci=906&stid_b=1&cevent=imt%2Fhomepage%2Fcategory1%2F1");
+            request.AddCookie("cityname", "%E7%BB%B5%E7%AB%B9");
+            var response = client.Execute(request);
+            var mtlst = SimpleJson.SimpleJson.DeserializeObject<MtLst>(response.Content);
+            return mtlst;
+        }
+    }
+}
---
+++ KDM/Mtlst.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KDM
+{
+
+    public class MtLst
+    {
+        public int status { get; set; }
+        public Data data { get; set; }
+    }
+
+    public class Data
+    {
+        public Poilist poiList { get; set; }
+        public object[] tips { get; set; }
+        public string notification { get; set; }
+        public Webview webView { get; set; }
+        public Tracedata traceData { get; set; }
+    }
+
+    public class Poilist
+    {
+        public Poilist()
+        {
+            poiInfos = new List<Poiinfo>();
+        }
+        public int totalCount { get; set; }
+        public List<Poiinfo> poiInfos { get; set; }
+    }
+
+    public class Poiinfo
+    {
+        public int avgPrice { get; set; }
+        public float avgScore { get; set; }
+        public string cateName { get; set; }
+        public string channel { get; set; }
+        public string showType { get; set; }
+        public string frontImg { get; set; }
+        public float lat { get; set; }
+        public float lng { get; set; }
+        public string name { get; set; }
+        public string poiid { get; set; }
+        public string areaName { get; set; }
+        public string iUrl { get; set; }
+        public Poiimgextra poiImgExtra { get; set; }
+        public Extraservicetag[] extraServiceTags { get; set; }
+        public Smarttag[] smartTags { get; set; }
+        public Preferentialinfo preferentialInfo { get; set; }
+        public string ctPoi { get; set; }
+        public string distance { get; set; }
+    }
+
+    public class Poiimgextra
+    {
+    }
+
+    public class Preferentialinfo
+    {
+        public int hiddenNumber { get; set; }
+        public Maidan maidan { get; set; }
+        public Combo combo { get; set; }
+    }
+
+    public class Maidan
+    {
+        public int defaultShowNum { get; set; }
+        public Entry[] entries { get; set; }
+    }
+
+    public class Entry
+    {
+        public string discount { get; set; }
+        public string discountColor { get; set; }
+        public string content { get; set; }
+        public string icon { get; set; }
+        public string promotion { get; set; }
+        public int hasBorder { get; set; }
+        public string borderColor { get; set; }
+    }
+
+    public class Combo
+    {
+        public int defaultShowNum { get; set; }
+        public object[] entries { get; set; }
+    }
+
+    public class Extraservicetag
+    {
+        public string icon { get; set; }
+        public Text text { get; set; }
+    }
+
+    public class Text
+    {
+        public string content { get; set; }
+        public string color { get; set; }
+        public string backgroundColor { get; set; }
+    }
+
+    public class Smarttag
+    {
+        public string icon { get; set; }
+        public Text1 text { get; set; }
+    }
+
+    public class Text1
+    {
+        public string content { get; set; }
+        public string color { get; set; }
+        public string backgroundColor { get; set; }
+    }
+
+    public class Webview
+    {
+        public string viewType { get; set; }
+        public string viewUrl { get; set; }
+        public int position { get; set; }
+        public string impressionUrl { get; set; }
+        public object[] impressionUrls { get; set; }
+    }
+
+    public class Tracedata
+    {
+        public string _apimeishi_rerank_report_data { get; set; }
+    }
+
+}
